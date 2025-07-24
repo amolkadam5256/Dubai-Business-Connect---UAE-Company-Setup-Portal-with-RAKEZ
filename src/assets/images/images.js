5c3178d1a52ebe2da45d7ebd62403c88cee93229
amolkadam5256 <amolkadam1274@gmail.com>
2025-07-24 12:31:17 +0530

    first commit

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -5,6 +5,11 @@ import Seo from './Seo.png';
 import favicon from './favicon.png';
 import logoSlider1 from './logoSlider1.png';
 import logoSlider2 from './logoSlider2.jpeg';
+import androidchrome from './androidchrome.png';
+import androidchrome2 from './androidchrome2.png';
+import appletouch from './appletouch.png';
+import favicon16 from './favicon16.png';
+import favicon32 from './favicon32.png';
 export const Images = {
   DestopBg,
   graph,
@@ -13,4 +18,9 @@ export const Images = {
   favicon,
   logoSlider1,
   logoSlider2,
+  androidchrome,
+  androidchrome2,
+  appletouch,
+  favicon16,
+  favicon32,
 };
